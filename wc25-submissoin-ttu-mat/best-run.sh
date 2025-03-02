d94331d27d244d00e8ef8b2022f6c0fff7adf936
Graysen Gould <gouldgraysen@gmail.com>
2025-03-02 17:56:22 -0600

    some changes

---
+++ wc25-submissoin-ttu-mat/best-run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=WRF
+#SBATCH --output=conus-%j-12.out
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=12
+#SBATCH --exclusive
+
+cd /fsx/conus_12km/
+
+export I_MPI_OFI_LIBRARY_INTERNAL=0
+spack load intel-oneapi-mpi intel-oneapi-compilers
+spack load wrf
+module load libfabric-aws
+wrf_exe=$(spack location -i wrf)/run/wrf.exe
+set -x
+ulimit -s unlimited
+ulimit -a
+
+export OMP_NUM_THREADS=8
+export FI_PROVIDER=efa
+export I_MPI_FABRICS=shm:ofi
+export I_MPI_OFI_PROVIDER=efa
+export I_MPI_PIN_DOMAIN=omp
+export KMP_AFFINITY=compact
+export I_MPI_DEBUG=4
+
+time mpiexec.hydra -np $SLURM_NTASKS --ppn $SLURM_NTASKS_PER_NODE $wrf_exe
